c22dfcb78b886d6e4869029d0f0af701322e4aa7
0leodev <leojimdev@gmail.com>
2024-09-29 21:01:20 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const clientId = import.meta.env.VITE_CLIENT_ID_GOOGLE;
-console.log(clientId);
 
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={clientId}>
